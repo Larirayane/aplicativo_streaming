e39b8ac6fc5fb63e76fc3633b387984753fa00a7
Larissa Rayane <larissarayane.francisco@gmail.com>
2024-03-14 11:16:49 -0300

    first commit

---
+++ app/src/main/java/com/example/globoplay/Home.kt
@@ -0,0 +1,15 @@
+package com.example.globoplay
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
